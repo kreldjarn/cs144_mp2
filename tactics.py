232e24d1b34e0b961dbfbd6b25f517d45da3a1cc
Kristján Eldjárn <kristjan@eldjarn.net>
2018-02-17 13:24:41 -0800

    Print out eigenvalue centrality

--- tactics.py
+++ tactics.py
@@ -8,16 +8,21 @@ import json
 # Third-party dependencies
 # ========================
 import networkx as nx
+import numpy as np
 
 def parse_graph(adj_list):
     G = nx.Graph()
-    for key in graph.keys():
+    for key in adj_list.keys():
         G.add_edges_from([(key, val) for val in adj_list[key]])
     return G
 
 def tactics(adj_list):
     G = parse_graph(adj_list)
-    print(G)
+    print('Total number of nodes: {}'.format(G.number_of_nodes()))
+    centrality = nx.eigenvector_centrality(G)
+    c_nodes = [k for k in sorted(centrality, key=centrality.get, reverse=True)]
+    for i in range(10):
+        print(c_nodes[i])
 
 
 if __name__ == '__main__':
