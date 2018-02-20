572cd7c829466abe2935dcb11916bbe12cb88693
Kristján Eldjárn <kristjan@eldjarn.net>
2018-02-19 16:42:54 -0800

    Simplified command line arguments

--- tactics.py
+++ tactics.py
@@ -27,26 +27,25 @@ def parse_graph(adj_list):
         G.add_edges_from([(key, val) for val in adj_list[key]])
     return G
 
-def selector(G,num_seeds,best_boys):
+def selector(G,n_seeds,best_boys):
 
-    #input variables: bestboys, G, num_seeds
+    #input variables: bestboys, G, n_seeds
     focus = best_boys[0][0]
     i=1
     seeds = []
-    while len(seeds) < num_seeds:
+    while len(seeds) < n_seeds:
         if random.random() < 0.8:
             seeds.append(focus)
         seeds.extend([n for n in G[focus] if random.random() < 0.8])
-        seeds = seeds[:num_seeds]
+        seeds = seeds[:n_seeds]
         focus = best_boys[i][0]
         i += 1
     return seeds
 
 
-def tactics_1st_gen(adj_list, outfile, num_seeds, n_players):
+def tactics_1st_gen(adj_list, outfile, n_seeds, n_players):
     N = 100
     G = parse_graph(adj_list)
-    print('Total number of nodes: {}'.format(G.number_of_nodes()))
     centrality = nx.eigenvector_centrality(G)
     central_nodes = [k for k in sorted(centrality, key=centrality.get, reverse=True)]
     print('most central: {}'.format(','.join([central_nodes[i] for i in range(10)])))
@@ -61,26 +60,24 @@ def tactics_1st_gen(adj_list, outfile, num_seeds, n_players):
 
     final_selections = []
     for i in range(50):
-        selections = dict((str(i), selector(G, num_seeds, best_boys[i:])) for i in range(n_players))
+        selections = dict((str(i), selector(G, n_seeds, best_boys[i:])) for i in range(n_players))
         simulation = sim.run(adj_list, selections)
         best = [k for k in sorted(simulation, key=simulation.get, reverse=True)][0]
         final_selections.extend(selections[best])
-    print(final_selections)
-    print(len(final_selections))
     print_to_file(final_selections, outfile)
 
-def tactics_2nd_gen(adj_list, outfile, num_seeds, n_players):
+def tactics_2nd_gen(adj_list, outfile, n_seeds, n_players):
     G = parse_graph(adj_list)
     degrees = G.degree()
     top_degree = [k[0] for k in sorted(degrees, key=lambda t: t[1], reverse=True)]
 
-    TA_set = set(top_degree[:num_seeds])
+    TA_set = set(top_degree[:n_seeds])
 
     final_seeds = []
     for i in range(50):
         idx = 0
         seeds = []
-        while len(seeds) < num_seeds:
+        while len(seeds) < n_seeds:
             seeds.append(top_degree[idx])
             nbrs = list(set(G[top_degree[idx]]) - set(seeds))
 
@@ -89,23 +86,22 @@ def tactics_2nd_gen(adj_list, outfile, num_seeds, n_players):
 
             # n_nbrs = int(math.floor(len(G[top_degree[idx]]) / 2) + 1)
             seeds.extend(np.random.choice(nbrs, size=n_nbrs))
-            seeds = seeds[:num_seeds]
+            seeds = seeds[:n_seeds]
             idx += 1
         final_seeds.extend(seeds)
-    print(len(final_seeds))
     print_to_file(final_seeds, outfile)
 
-def tactics_degree(adj_list, outfile, num_seeds, n_players):
+def tactics_degree(adj_list, outfile, n_seeds, n_players):
     G = parse_graph(adj_list)
     degrees = G.degree()
     top_degree = [k[0] for k in sorted(degrees, key=lambda t: t[1], reverse=True)]
 
-    TA_set = top_degree[:num_seeds]
+    TA_set = top_degree[:n_seeds]
     neighbors_of_top_dog = G[TA_set[0]]
     top_neighbors = [t for t in TA_set if t in neighbors_of_top_dog]
-    seeds = TA_set[:num_seeds-1]
+    seeds = TA_set[:n_seeds-1]
 
-    TA_set = top_degree[:num_seeds]
+    TA_set = top_degree[:n_seeds]
     best = 0
     final_seeds = []
     for n in G[TA_set[0]]:
@@ -119,12 +115,12 @@ def tactics_degree(adj_list, outfile, num_seeds, n_players):
     print(sim.run(adj_list, {'us': final_seeds, 'them': TA_set}))
     print_to_file(final_seeds * 50, sys.argv[2])
 
-def tactics_fewer(adj_list, outfile, num_seeds, n_players):
+def tactics_fewer(adj_list, outfile, n_seeds, n_players):
     G = parse_graph(adj_list)
     degrees = G.degree()
     top_degree = [k[0] for k in sorted(degrees, key=lambda t: t[1], reverse=True)]
 
-    TA_set = top_degree[:num_seeds - 2]
+    TA_set = top_degree[:n_seeds - 2]
 
     neighbors_of_top_dog = G[TA_set[0]]
     # nbrs = [k[0] for k in sorted(G.degree(G[TA_set[0]]), key=lambda t: t[1], reverse=True) if k[0] not in seeds and k[0] not in TA_set]
@@ -134,7 +130,7 @@ def tactics_fewer(adj_list, outfile, num_seeds, n_players):
     seeds = set(TA_set)
     nbrs = [k[0] for k in sorted(G.degree(G[TA_set[0]]), key=lambda t: t[1], reverse=True)]
     i = 0
-    while len(seeds) < num_seeds:
+    while len(seeds) < n_seeds:
         seeds.add(nbrs[i])
         i += 1
 
@@ -149,7 +145,7 @@ if __name__ == '__main__':
     """
     Usage:
 
-    python3 tactics.py name_of_strategy input_file number_of_seeds number_of_players
+    python3 tactics.py name_of_strategy input_file
 
     """
     funcs = {
@@ -162,10 +158,17 @@ if __name__ == '__main__':
     try:
         with open(sys.argv[2], 'r') as fh:
             adj_list = json.loads(fh.read())
+        outfile = sys.argv[2].split('/')[1].rsplit('.', 1)[0]
+        params = outfile.split('.')
+        n_players = int(params[0])
+        n_seeds = int(params[1])
+        print('Number of nodes: {}'.format(len(adj_list)))
+        print('Number of players: {}'.format(n_players))
+        print('Number of seeds: {}'.format(n_seeds))
         funcs[sys.argv[1]](adj_list,
-                           'results/{}'.format(sys.argv[2].split('/')[1]),
-                           int(sys.argv[3]),
-                           int(sys.argv[4]))
+                           'results/{}.txt'.format(outfile),
+                           n_seeds,
+                           n_players)
     except IndexError as e:
         print('FEED ME A GRAPH')
         print('     (ಠ‿ಠ)     ')
